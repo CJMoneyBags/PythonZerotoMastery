3882d124be221e6e4e60234a07f6d37dd01ac0dd
Chris Bagwell <49886936+CJMoneyBags@users.noreply.github.com>
2026-02-11 20:50:49 -0700

    initial

---
+++ highestEven.py
@@ -0,0 +1,21 @@
+def highest_even(li):
+#    maxNum = 0
+#    for item in li:
+#       if item % 2 == 0:
+#          if maxNum > item:
+#             return maxNum
+#          else:
+#             maxNum = item
+#             return maxNum
+    evens = []
+    for items in li:
+        if items % 2 == 0:
+            evens.append(items)
+    if evens == []:
+        return "No evens in list"
+    else:
+        return max(evens)
+
+        
+
+print(highest_even([1,23,33,43,81,11]))
